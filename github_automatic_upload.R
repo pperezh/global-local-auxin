0ccde6d1a7d2b2a397f2d67115524df09f2d17f3
Patricio Pérez-Henríquez <80292580+pperezh@users.noreply.github.com>
2024-06-05 11:28:16 -0700

    Update github_automatic_upload.R

--- github_automatic_upload.R
+++ github_automatic_upload.R
@@ -1,22 +1,16 @@
 #### Automatic upload of Data - excluding lobes-table.txt ####
 directoriesAll <- list.dirs(full.names = TRUE ,recursive = FALSE)
-directoriesOutput <-grep("output*",directoriesAll, value=TRUE)            #  This line is to remove "output*" folders. if present
+directoriesOutput <-grep("output*",directoriesAll, value=TRUE)            #  This line is to remove PaCeQuantAna "output*" folders
 directories <- setdiff(directoriesAll,directoriesOutput)
-directories <- str_sub(directories,3)                           # This is to remove "./" from each folder name
+directories <- str_sub(directories,3)                                      # This is to remove "./" from each folder name
 Newcolumn <- str_split(directories, "_",simplify=TRUE)
 
 longlist <- list.files(path = paste(directories[1], "results", sep ="/"), pattern = "*-table.txt", full.names = TRUE)
 shortlist <- list.files(path = paste(directories[1], "results", sep ="/"), pattern = "*-lobe-table.txt", full.names = TRUE)
 filelist <- setdiff(longlist,shortlist) 
 mydataframe <- read.table(file = filelist[1], header = TRUE, sep = "\t")
-mydataframe$Genotype <- Newcolumn [1,1]                         ### Change to Time| Position | Genotype, etc,  if needed
-mydataframe$Treatment <- Newcolumn [1,2]                        ##### Change to Time| Position | Genotype, etc,  if needed
-#df <- read.table(filelist[1], header=T, sep = "\t")
-#df$Genotype <- NA
-#df$Treatment <- NA
-#df$Genotype <- Newcolumn [1,1]                                  ### Change accordingly
-#df$Treatment <- Newcolumn [1,2]
-
+mydataframe$Genotype <- Newcolumn [1,1]                         ### Change accordingly (to Time, Position, Genotype, etc,  if needed
+mydataframe$Treatment <- Newcolumn [1,2]                        ### Change accordingly (to Time, Position, Genotype, etc,  if needed
 
 for (i in 1:length(directories))
 {
@@ -30,8 +24,8 @@ for (i in 1:length(directories))
     if(i == 1 && f == 1) next
     #df <- map_df (filelist, read.table, header = T)
     df <- read.table(filelist[f], header=T, sep = "\t")
-    df$Genotype <- Newcolumn [i,1]                             ### Change accordingly
-    df$Treatment <- Newcolumn [i,2]                            #### Change accordingly
+    df$Genotype <- Newcolumn [i,1]                             ### Change accordingly (to Time, Position, Genotype, etc,  if needed
+    df$Treatment <- Newcolumn [i,2]                            ### Change accordingly (to Time, Position, Genotype, etc,  if needed
     mydataframe <- rbind(mydataframe,df)
   }
 }
@@ -40,8 +34,8 @@ mydataframe["Lobes per Area*1000"] <- mydataframe$LobeCount/mydataframe$Area_.mi
 mydataframe["Lobes per Perimeter*1000"] <- mydataframe$LobeCount/mydataframe$Perimeter_.microns.*1000
 mydataframe["MR per Area*1000"] <- mydataframe$MarginRoughness/mydataframe$Area_.microns.2.*1000
 mydataframe["MR per Perimeter*1000"] <- mydataframe$MarginRoughness/mydataframe$Perimeter_.microns.*1000
-mydataframe$Treatment <- as.factor(mydataframe$Treatment)  
-mydataframe$Genotype <- as.factor(mydataframe$Genotype) 
+mydataframe$Treatment <- as.factor(mydataframe$Treatment)      ### Change accordingly (to Time, Position, Genotype, etc,  if needed
+mydataframe$Genotype <- as.factor(mydataframe$Genotype)        ### Change accordingly (to Time, Position, Genotype, etc,  if needed
 
 
 
